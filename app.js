ec4869ac3162b8bd66f27cb2623f0f690c557547
Abhishek Kumar <abhishekkumar500a@gmail.com>
2022-01-11 18:10:00 +0530

    modify app.js

--- app.js
+++ app.js
@@ -11,20 +11,43 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
+const dateOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+
+const timeOptions = {
+  hour: '2-digit',
+  minute: '2-digit'
+};
+
+let query = "City Name";
+let weatherDesc = "Description";
+let temp = "Temp";
+let imageURL = "";
+
 app.get("/", function(req, res){
+
+  const raw_today = new Date();
+  const today = convertTZ(raw_today, "Asia/Kolkata");
+  const date = today.toLocaleDateString("en-US", dateOptions);
+  const time = today.toLocaleTimeString("en-US", timeOptions);
+
   res.render("index", {
-    city: "City Name",
-    temp: "Temp",
-    weatherIcon: "Image",
-    weatherDesc: "Description",
-    time: moment().format('LT'),
-    date: moment().format('dddd') + ", " + moment().format('LL')
+    city: _.upperCase(query),
+    temp: temp,
+    weatherIcon: imageURL,
+    weatherDesc: _.startCase(weatherDesc),
+    time: time,
+    date: date
   });
 });
 
 app.post("/", function(req, res){
 
-  const query = req.body.cityName;
+  query = req.body.cityName;
   const appKey = "1b2524ed520e1c658ec83fd4b04a8325";
   const unit = "metric";
   const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ appKey +"&units=" + unit;
@@ -37,23 +60,29 @@ app.post("/", function(req, res){
     } else {
       response.on("data", function(data){
         const weatherData = JSON.parse(data);
-        const weatherDesc = weatherData.weather[0].description;
+        weatherDesc = weatherData.weather[0].description;
         const icon = weatherData.weather[0].icon;
-        const temp = weatherData.main.temp;
-        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        temp = weatherData.main.temp;
+        imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-        res.render("index", {
-          city: _.upperCase(query),
-          temp: temp,
-          weatherIcon: imageURL,
-          weatherDesc: _.startCase(weatherDesc),
-          time: moment().format('LT'),
-          date: moment().format('dddd') + ", " + moment().format('LL')
-        });
+        res.redirect("/");
+
+        // res.render("index", {
+        //   city: _.upperCase(query),
+        //   temp: temp,
+        //   weatherIcon: imageURL,
+        //   weatherDesc: _.startCase(weatherDesc),
+        //   time: moment().format('LT'),
+        //   date: moment().format('dddd') + ", " + moment().format('LL')
+        // });
       });
     }
   });
 });
+
+function convertTZ(date, tzString) {
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
+}
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server is Started");
